662461df15f1813d4071f6b286b32e1dbec6b0ae
zlc1994 <me@zlc1994.com>
2019-05-20 21:17:09 +0800

    update

--- src/main/java/_0002/Solution.java
+++ src/main/java/_0002/Solution.java
@@ -21,35 +21,26 @@ class Solution {
         ListNode p = head;
         int carry = 0;
 
-        while (l1 != null && l2 != null) {
-            int sum = l1.val + l2.val + carry;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
             p.next = new ListNode(sum % 10);
             p = p.next;
-            l1 = l1.next;
-            l2 = l2.next;
             carry = sum / 10;
-        }
 
-        while (l1 != null) {
-            int sum = l1.val + carry;
-            p.next = new ListNode(sum % 10);
-            p = p.next;
-            l1 = l1.next;
-            carry = sum / 10;
-        }
+            if (l1 != null) {
+                l1 = l1.next;
+            }
 
-        while (l2 != null) {
-            int sum = l2.val + carry;
-            p.next = new ListNode(sum % 10);
-            p = p.next;
-            l2 = l2.next;
-            carry = sum / 10;
+            if (l2 != null) {
+                l2 = l2.next;
+            }
         }
 
         if (carry != 0) {
             p.next = new ListNode(carry);
         }
-
         return head.next;
     }
 }
