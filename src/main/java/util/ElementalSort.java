186555decf600b9c4e1c53f6b14df3b79bacf7cd
zlc <me@zlc1994.com>
2019-05-27 15:22:17 +0800

    update

--- src/main/java/util/ElementalSort.java
+++ src/main/java/util/ElementalSort.java
@@ -1,4 +1,23 @@
 package util;
 
 public class ElementalSort {
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j+1]) {
+                    SortUtil.swap(array, j, j+1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = SortUtil.newRandomArray(100);
+
+        bubbleSort(array);
+
+        System.out.println(SortUtil.isSorted(array));
+    }
 }
